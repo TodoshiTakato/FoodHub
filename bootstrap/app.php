094196f6e4da0dd5001af82b8ae3ea10cf99f3ac
Weed <44820183+TodoshiTakato@users.noreply.github.com>
2025-05-20 12:21:58 +0500

    main branch: installed passport

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,13 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        api: __DIR__.'/../routes/api.php',
+        using: function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        },
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json(['message' => 'FoodHub API']);
-});
+Route::get(uri: '/user', action: function (Request $request) {
+    return $request->user();
+})->middleware(middleware: 'auth:api');
